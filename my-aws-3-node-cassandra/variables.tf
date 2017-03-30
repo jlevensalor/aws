971004eca2f2a4d0f171e3551d20751cc512cc63
Jerrett Levensalor <jlevensalor@gmail.com>
2017-03-30 11:12:58 -0700

    Removing user_data

--- my-aws-3-node-cassandra/variables.tf
+++ my-aws-3-node-cassandra/variables.tf
@@ -1,7 +1,6 @@
 variable "user_name" { default = "ubuntu" }
 variable "ssh_key_path" { default = "~/.ssh/cassandra.pem" }
 variable "ssh_key_name" { default = "cassandra" }
-variable "cass_setup_file" { default = "provisioning/setup_cassandra.sh" }
 variable "cidr" { default = "10.2.4.0/23" }
 variable "instance_type" { default = "t2.small" }
 variable "security_group_name" { default = "terraform" }
